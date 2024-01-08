b162c67357848958884234d7117bd5a474e57235
Ratan Kumar <111689315+RatanKumar01@users.noreply.github.com>
2024-01-08 21:29:19 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+
+    repositories {
+
+        google()
+    }
+}
+
+rootProject.name = "MemeShare"
+include(":app")
